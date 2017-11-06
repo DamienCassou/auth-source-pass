501739995ab67ec1d6433ac2432b7e5d951b56a3
Edison Ibañez <arkhan.xxx@gmail.com>
2017-11-05 20:40:57 -0500

    FIX Test

--- test/auth-password-store-tests.el
+++ test/auth-password-store-tests.el
@@ -178,7 +178,7 @@ test code without touching the file system."
   (should (equal (auth-pass--hostname "https://SomeUser@foo.bar") "foo.bar")))
 
 (ert-deftest hostname-with-user ()
-  (should (equal (auth-pass--hostname-with-user "https://foo.bar:443") "foo.bar:443"))
+  (should (equal (auth-pass--hostname-with-user "https://foo.bar:443") "foo.bar"))
   (should (equal (auth-pass--hostname-with-user "https://foo.bar") "foo.bar"))
   (should (equal (auth-pass--hostname-with-user "http://foo.bar") "foo.bar"))
   (should (equal (auth-pass--hostname-with-user "https://SomeUser@foo.bar") "SomeUser@foo.bar")))
