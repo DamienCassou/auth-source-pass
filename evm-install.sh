552a64440d5e1f87603ba7a47d8a9c9ec877b9ec
Damien Cassou <damien@cassou.me>
2019-01-04 09:23:12 +0100

    Remove the need for evm-install-sh

--- evm-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export PATH="/home/travis/.evm/bin:$PATH"
-
-git clone https://github.com/rejeep/evm.git /home/travis/.evm
-evm config path /tmp
-evm install emacs-25.3-travis --use --skip
